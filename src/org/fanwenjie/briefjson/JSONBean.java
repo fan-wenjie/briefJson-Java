c705ecd7a4cf863a09c595553cc4aabaff9f6f78
Fan Wen Jie <fanwenjie1991@gmail.com>
2015-03-06 11:07:32 +0800

    Add Type Check

--- src/org/fanwenjie/briefjson/JSONBean.java
+++ src/org/fanwenjie/briefjson/JSONBean.java
@@ -11,23 +11,23 @@ import java.util.*;
  * @version 2015-03-05
  */
 public class JSONBean {
-    public static Object serialize(Object bean) throws NullPointerException{
-        if(bean == null)
+    public static Object serialize(Object bean) throws NullPointerException {
+        if (bean == null)
             return null;
-        if(bean instanceof String||bean instanceof Boolean||bean instanceof Number)
+        if (bean instanceof String || bean instanceof Boolean || bean instanceof Number)
             return bean;
-        if(bean instanceof Collection)
-            return serialize(((Collection)bean).toArray());
-        if(bean.getClass().isArray()){
+        if (bean instanceof Collection)
+            return serialize(((Collection) bean).toArray());
+        if (bean.getClass().isArray()) {
             int length = Array.getLength(bean);
             ArrayList<Object> array = new ArrayList<Object>(length);
-            for(int i=0;i<length;++i)
+            for (int i = 0; i < length; ++i)
                 array.add(serialize(Array.get(bean, i)));
             return array;
         }
-        if(bean instanceof Map){
-            Map map = (Map)bean;
-            for(Object key : map.keySet())
+        if (bean instanceof Map) {
+            Map map = (Map) bean;
+            for (Object key : map.keySet())
                 map.put(key, serialize(map.get(key)));
             return map;
         }
@@ -49,22 +49,22 @@ public class JSONBean {
                         key = seriable.name();
                     int order = seriable.order();
                     int positon = indexs.size();
-                    if(order<Integer.MAX_VALUE)
-                        for(int i=0;i<indexs.size();++i)
-                            if(order<indexs.get(i))
+                    if (order < Integer.MAX_VALUE)
+                        for (int i = 0; i < indexs.size(); ++i)
+                            if (order < indexs.get(i))
                                 positon = i;
-                    indexs.add(positon,order);
+                    indexs.add(positon, order);
                     values.add(positon, value);
-                    keys.add(positon,key);
+                    keys.add(positon, key);
                 }
             } catch (Exception ignore) {
             } finally {
-                if(isRequired&&null==value)
-                    throw new NullPointerException("Field "+field.getName()+" can't be null");
+                if (isRequired && null == value)
+                    throw new NullPointerException("Field " + field.getName() + " can't be null");
             }
         }
-        LinkedHashMap<String,Object> map = new LinkedHashMap<String, Object>(indexs.size());
-        for(int i=0;i<indexs.size();++i)
+        LinkedHashMap<String, Object> map = new LinkedHashMap<String, Object>(indexs.size());
+        for (int i = 0; i < indexs.size(); ++i)
             map.put(keys.get(i), values.get(i));
         return map;
     }
@@ -97,65 +97,58 @@ public class JSONBean {
                             value = deserialize(clazz, genericType, (Object[]) value);
                         else return null;
                     } else
-                        value = deserialize(clazz,value);
+                        value = deserialize(clazz, value);
                     field.set(bean, value);
                 }
-            }catch (Exception ignore){}
-            finally {
-                if(isRequired&&value==null)
+            } catch (Exception ignore) {
+            } finally {
+                if (isRequired && value == null)
                     throw new NullPointerException();
             }
         }
-        return (T)bean;
+        return (T) bean;
     }
 
-    public static <T,A> Collection<T> deserialize(Class<? extends Collection> klass,Class<T> genericType,A[] array) throws Exception{
+    public static <T,A> Collection<T> deserialize(Class<? extends Collection> klass,Class<T> genericType,A[] array) throws Exception {
         Collection collection = klass.newInstance();
-        for (int i=0;i<array.length;++i)
-            collection.add(deserialize(genericType,array[i]));
+        for (int i = 0; i < array.length; ++i)
+            collection.add(deserialize(genericType, array[i]));
         return collection;
     }
 
-    public static <T> Collection<T> deserialize(Class<? extends Collection> klass,Class<T> genericType,Collection array) throws Exception{
-        return deserialize(klass,genericType,array.toArray());
+    public static <T> Collection<T> deserialize(Class<? extends Collection> klass,Class<T> genericType,Collection array) throws Exception {
+        return deserialize(klass, genericType, array.toArray());
     }
 
-    public static <T> T[] deserialize(Class<T> componentType,Collection array) throws Exception{
-        return deserialize(componentType,array.toArray());
+    public static <T> T[] deserialize(Class<T> componentType,Collection array) throws Exception {
+        return deserialize(componentType, array.toArray());
     }
 
-    public static <T,A> T[] deserialize(Class<T> componentType,A[] array) throws Exception{
-        T[] collection = (T[])(Array.newInstance(componentType,array.length));
-        for(int i=0;i<array.length;++i)
-            collection[i] = deserialize(componentType,array[i]);
+    public static <T,A> T[] deserialize(Class<T> componentType,A[] array) throws Exception {
+        T[] collection = (T[]) (Array.newInstance(componentType, array.length));
+        for (int i = 0; i < array.length; ++i)
+            collection[i] = deserialize(componentType, array[i]);
         return collection;
     }
 
-    public static<T> T deserialize(Class<T> klass,Object object) throws Exception{
-        if(object instanceof Number || object instanceof String || object instanceof Boolean)
-            return (T)object;
-        else if(object instanceof Map)
-            return (T)deserialize(klass, (Map) object);
-        else if(Collection.class.isAssignableFrom(klass)) {
+    public static<T> T deserialize(Class<T> klass,Object object) throws Exception {
+        if (object instanceof Number || object instanceof String || object instanceof Boolean)
+            return (T) object;
+        else if (object instanceof Map)
+            return (T) deserialize(klass, (Map) object);
+        else if (Collection.class.isAssignableFrom(klass)) {
             if (object instanceof Collection)
-
-            return (T)deserialize((Class<? extends Collection>)klass, Object.class, (Collection) object);
+                return (T) deserialize((Class<? extends Collection>) klass, Object.class, (Collection) object);
             else if (object.getClass().isArray())
-                return (T)deserialize((Class<? extends Collection>)klass, Object.class, (Object[]) object);
+                return (T) deserialize((Class<? extends Collection>) klass, Object.class, (Object[]) object);
             else return null;
-        }else if (klass.isArray()) {
+        } else if (klass.isArray()) {
             if (object instanceof Collection)
-                return (T)deserialize(klass.getComponentType(), (Collection) object);
+                return (T) deserialize(klass.getComponentType(), (Collection) object);
             else if (object.getClass().isArray())
-                return (T)deserialize(klass.getComponentType(), (Object[]) object);
+                return (T) deserialize(klass.getComponentType(), (Object[]) object);
             else return null;
-        }else
+        } else
             return null;
     }
-
-
-
-
-
-
 }
